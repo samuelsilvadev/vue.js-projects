e45d0beda71488136529f0de526e277a662ea3f5
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-09 09:09:14 +0100

    chore(private-pictures): Create PhotoPanel component
    - Created component with isolated css.

---
+++ private-pictures/src/components/shared/PhotoPanel.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="photo-panel">
+    <h2 class="photo-panel__title">{{ title }}</h2>
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style scoped>
+.photo-panel {
+  border: solid 2px grey;
+  box-shadow: 5px 5px 10px grey;
+  display: inline-block;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  vertical-align: top;
+  width: 100%;
+}
+
+.photo-panel__title {
+  background-color: lightblue;
+  border: 2px solid;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-align: center;
+  text-transform: uppercase;
+}
+</style>
+
