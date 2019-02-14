368a91cb5aa5eb7fda50474add875d50e9e05725
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-14 19:59:01 +0100

    chore(private-pictures): Add vue router
    - Installed module.
    - Registered module on main.js.

--- private-pictures/src/main.js
+++ private-pictures/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import VueResource from "vue-resource";
+import VueRouter from "vue-router";
 import App from "./App.vue";
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
