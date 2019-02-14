069075ddccef982237b3e9a1d8ce73f96289a4c7
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-14 19:01:23 +0100

    feat(private-pictures): Add photos filter
    - Added function to filter photos by title.
    - Added line on common.css

--- private-pictures/src/App.vue
+++ private-pictures/src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
     <h1 class="title">Private Pictures</h1>
+    <input
+      class="search-field"
+      type="search"
+      name="photos-list-search"
+      placeholder="Search for a title..."
+      v-on:input="filterValue = $event.target.value"
+    />
     <ul class="photos-list">
-      <li class="photos-list__item" v-for="photo of photos" :key="photo.id">
+      <li
+        class="photos-list__item"
+        v-for="photo of filteredPhotos"
+        :key="photo.id"
+      >
         <photo-panel :title="photo.title">
           <img :src="photo.url" :alt="photo.title" />
         </photo-panel>
@@ -19,9 +30,22 @@ export default {
   components: {
     "photo-panel": PhotoPanel
   },
+  computed: {
+    filteredPhotos() {
+      if (!this.filterValue) {
+        return this.photos;
+      }
+
+      const regExp = new RegExp(this.filterValue.trim(), "i");
+      const photos = this.photos.filter(photo => regExp.test(photo.title));
+
+      return photos;
+    }
+  },
   data() {
     return {
-      photos: []
+      photos: [],
+      filterValue: ""
     };
   },
   created() {
@@ -47,6 +71,20 @@ export default {
   font-weight: bold;
   text-align: center;
   margin: 30px auto;
+}
+
+.search-field {
+  border: 1px solid gray;
+  border-radius: 4px;
+  display: block;
+  height: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10px;
+  text-align: center;
+  text-transform: uppercase;
+  width: 50%;
+  -webkit-appearance: none;
 }
 
 .photos-list {
