3f1dbf9be7314b61593e8544ffb7861bd607f5ed
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-15 20:26:42 +0100

    refactor(private-pictures): Create nav item dynamically when a new route be added
    - Added logic on App.vue to render dynamically routes.

--- private-pictures/src/App.vue
+++ private-pictures/src/App.vue
@@ -1,10 +1,29 @@
 <template>
   <div id="app">
+    <nav>
+      <ul>
+        <li v-for="route of routes" :key="route.path">
+          <router-link :to="route.path ? route.path : '/'">{{
+            route.title
+          }}</router-link>
+        </li>
+      </ul>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
-<script></script>
+<script>
+import { routes } from "./routes";
+
+export default {
+  data() {
+    return {
+      routes
+    };
+  }
+};
+</script>
 
 <style>
 @import "./styles/common.css";
--- private-pictures/src/routes.js
+++ private-pictures/src/routes.js
@@ -2,6 +2,6 @@ import Home from "./components/home/Home";
 import Register from "./components/register/Register";
 
 export const routes = [
-  { path: "", component: Home },
-  { path: "/register", component: Register }
+  { path: "", component: Home, title: "Home" },
+  { path: "/register", component: Register, title: "Register" }
 ];
